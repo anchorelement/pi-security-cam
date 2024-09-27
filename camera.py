77a49d9308bf1fedd8803772ee4b622a56afe2fd
anchorelement <github.boxlike807@passmail.net>
2024-09-27 11:01:40 -0600

    Added functionality to stop recording after a certain lenght of time (break up the video files).

--- camera.py
+++ camera.py
@@ -19,8 +19,9 @@ logger = logging.getLogger(__name__)
 
 LSIZE = (640, 480)  # size for the lores video config
 MSIZE = (1920, 1080)  # size for the main video config
-MOTION_SENS_THRESH = 100_000  # threshold for triggering motion capture, higher requires more change between frames
-RECORD_AFTER = 5.0  # how long to keep recording after motion passes below threshold
+MOTION_SENS_THRESH = 95_000  # threshold for triggering motion capture, higher requires more change between frames
+RECORD_TAIL = 5.0  # how long to keep recording after motion passes below threshold
+RECORD_MAX = 30.0  # maximum length of individual video files
 
 
 class Camera:
@@ -34,9 +35,8 @@ class Camera:
             lores={"size": LSIZE, "format": "YUV420"},
             raw=self.picam2.sensor_modes[1],
         )
-        self.motion_sense_thresh = MOTION_SENS_THRESH
-        self.record_after = RECORD_AFTER
         self.encoder = H264Encoder(bitrate=10000000)
+        self.encoding = False
         self.picam2.configure(video_config)
         self.picam2.start()
         # pprint(self.picam2.camera_configuration())
@@ -61,9 +61,8 @@ class Camera:
         gray2 = cv2.GaussianBlur(gray2, (21, 21), 0)
         diff = cv2.absdiff(gray1, gray2)
         cnz = cv2.countNonZero(diff)
-        # logging.info(cnz)
         if cnz > MOTION_SENS_THRESH:
-            logging.info(cnz)
+            logging.debug(f"Count of non-zero pixels in delta frame: {cnz}.")
             return True
         return False
 
@@ -74,8 +73,8 @@ class Camera:
 
     def run(self):
         previous = None
-        encoding = False
-        ltime = 0
+        self.encoding = False
+        start_time = 0
         motion_frames = 0
         while self.armed:
             current = self.picam2.capture_array("lores")
@@ -83,24 +82,42 @@ class Camera:
                 if self.detect_motion(current, previous):
                     motion_frames += 1
                     # filter out short spikes of detected motion (usually false positives)
-                    if not encoding and motion_frames > 2:
-                        pic_thread = threading.Thread(target=self.send_notification)
-                        pic_thread.start()
-                        self.picam2.start_encoder(
-                            encoder=self.encoder,
-                            output=FileOutput(
-                                f"video/{time.strftime('%Y_%m_%d-%H_%M_%S')}.h264"
-                            ),
-                            quality=Quality.HIGH,
-                            pts="video/timestamps.txt",
-                        )
-                        encoding = True
-                        logging.info("New motion.")
+                    if not self.encoding and motion_frames > 2:
+                        self.start_video()
+                        start_time = time.time()
+                    # mark last time of detected motion
                     ltime = time.time()
-                else:
-                    if encoding and time.time() - ltime > self.record_after:
-                        self.picam2.stop_encoder()
-                        encoding = False
-                        motion_frames = 0
-                        logging.info("Stopped encoding.")
+                    # check if video is over the max length
+                    if self.encoding:
+                        encoding_length = ltime - start_time
+                        logging.debug(f"Video recording for: {encoding_length:.3}s.")
+                        if encoding_length > RECORD_MAX:
+                            logging.info(
+                                f"Stopping video due to max length: {encoding_length:.3}s."
+                            )
+                            self.stop_video()
+                            motion_frames = 0
+                # check if video is still recording and we are over the tail length with no motion detected
+                elif self.encoding and time.time() - ltime > RECORD_TAIL:
+                    logging.info(
+                        f"Stopping video since no motion has been detected after: {time.time() - ltime:.3}s."
+                    )
+                    self.stop_video()
+                    motion_frames = 0
             previous = current
+
+    def start_video(self):
+        notification_thread = threading.Thread(target=self.send_notification)
+        notification_thread.start()
+        self.picam2.start_encoder(
+            encoder=self.encoder,
+            output=FileOutput(f"video/{time.strftime('%Y_%m_%d-%H_%M_%S')}.h264"),
+            quality=Quality.HIGH,
+        )
+        self.encoding = True
+        logging.info("New motion.")
+
+    def stop_video(self):
+        self.picam2.stop_encoder()
+        self.encoding = False
+        logging.info("Stopped encoding.")
